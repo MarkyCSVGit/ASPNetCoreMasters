1489b9941e11e7a141935e71e162b5d80dd268a2
MarkyCSVGit <markjun.delarosa@softvision.com>
2022-08-16 19:41:59 +0800

    session 2 homework

---
+++ ASPNetCoreMastersTodoList.Api/Controllers/ItemsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace ASPNetCoreMastersTodoList.Api.Controllers
+{
+    [Route("api/Items")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly ItemService _itemService;
+        public ItemsController(ItemService itemService)
+        {
+            _itemService = itemService ?? throw new ArgumentNullException(nameof(itemService));
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get() =>
+            _itemService.GetAll();
+
+    }
+}
--- Services/ItemService.cs
+++ Services/ItemService.cs
@@ -2,6 +2,10 @@
 {
     public class ItemService
     {
+        public IEnumerable<string> GetAll()
+        {
+            return new string[] { "value1", "value2" };
+        }
 
     }
 }
